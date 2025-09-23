8a00d07f42a67e7be8059530f4ea262bcda3b6c8
Naveed-Raees <naveedraees271@gmail.com>
2025-09-23 17:07:08 +0500

    updated

--- src/app/[locale]/studio/page.js
+++ src/app/[locale]/studio/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { useLocale, useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
 
 const StudioPage = () => {
   const locale = useLocale();
@@ -9,13 +10,18 @@ const StudioPage = () => {
   return (
     <>
       <div
+        style={
+          locale === "en"
+            ? { backgroundImage: "url('/studio-bg.jpg')" }
+            : undefined
+        }
         className={`${
           locale === "he" && "rtl"
-        } min-h-screen bg-white text-dark-gray px-4 sm:px-6 lg:px-8 py-12`}
+        } min-h-screen bg-white bg-cover bg-center  bg-no-repeat  text-dark-gray px-4 sm:px-6 lg:px-8 py-16`}
       >
-        <div>
+        <div className="space-y-96">
           {/* Header Section */}
-          <div className="mb-16">
+          <div className="">
             <h1 className="text-xl md:text-2xl font-bold mb-6 md:mb-8 uppercase">
               {t("header.title")}
             </h1>
@@ -29,32 +35,35 @@ const StudioPage = () => {
                 })}
               </p>
             </div>
+            <Button className="mt-8 bg-primary text-dark-gray uppercase px-10 rounded-full">
+              Book a Class
+            </Button>
           </div>
 
           {/* Location Section */}
-          <div className="mb-8">
-            <h1 className="text-xl md:text-2xl font-bold mb-6 md:mb-8 tracking-wider">
+          <div className=" bg-white py-4 px-10 rounded-3xl">
+            <h1 className="text-xl  font-bold mb-2 tracking-widest uppercase text-dark-gray">
               {t("location.title")}
             </h1>
 
-            <div className="text-sm text-gray-600 mb-6">
+            <div className="text-[12px] text-gray-600 mb-6 leading-tight">
               <p className="font-medium">{t("location.address1")}</p>
               <p>{t("location.address2")}</p>
             </div>
-          </div>
 
-          {/* Google Maps Embed */}
-          <div className="w-full h-96 overflow-hidden ">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.7878654321!2d34.7651538!3d32.0699771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4db7b434aca7%3A0x8d5a40c17e6a8773!2sStarlette!5e0!3m2!1sen!2s!4v1693580000000!5m2!1sen!2s"
-              width="100%"
-              height="100%"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              className="grayscale hover:grayscale-0 transition-all duration-300"
-            />
+            {/* Google Maps Embed */}
+            <div className="w-full h-96 overflow-hidden ">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.7878654321!2d34.7651538!3d32.0699771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4db7b434aca7%3A0x8d5a40c17e6a8773!2sStarlette!5e0!3m2!1sen!2s!4v1693580000000!5m2!1sen!2s"
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className="transition-all duration-300"
+              />
+            </div>
           </div>
         </div>
       </div>
